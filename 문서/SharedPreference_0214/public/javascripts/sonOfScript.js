b081fa91324ac9dc11c5bc780821e61a8aa72820
songtaeyang <sty0331@gmail.com>
2015-02-22 18:53:02 +0900

    friend lists onclick update - 태양 150222 PM06:52

--- 문서/SharedPreference_0214/public/javascripts/sonOfScript.js
+++ 문서/SharedPreference_0214/public/javascripts/sonOfScript.js
@@ -220,7 +220,7 @@ function initFriendsList(result){
 
         li_.onclick= function(){
             userInfo.setTargetUser(p_.textContent);
-            window.location.reload();
+            window.location.reload(true);
         };
         li_.onmouseover= function(){
             this.setAttribute('style', 'background-color:#999');
