7f6588fc03b2730b48335875881a9dd4fea4032f
songtaeyang <sty0331@gmail.com>
2015-02-22 21:08:48 +0900

    friend lists onclick update - 태양 150222 PM07:04

--- 문서/SharedPreference_0214/public/javascripts/sonOfScript.js
+++ 문서/SharedPreference_0214/public/javascripts/sonOfScript.js
@@ -220,7 +220,7 @@ function initFriendsList(result){
 
         //li_.onclick = changeRoom(this.id);
         li_.onmousedown = function(){
-            userInfo.setTargetUser(this.id);
+            userInfo.setTargetUser(this.p.textContent);
             //window.location.reload();
             //location.href = 'http://210.118.64.172:8000/#'+ userInfo.getTargetUser();
             //history.go(0);
